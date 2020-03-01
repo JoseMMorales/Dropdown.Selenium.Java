5e083f07c5e5b772ebff77a3591539492b3e8c06
Jose <josemulamorales33@hotmail.com>
2020-03-01 17:17:46 +0000

    Update  Dropdown Scripts code

--- src/DropdownScripts/AscendingAndDescendingOrder.java
+++ src/DropdownScripts/AscendingAndDescendingOrder.java
@@ -3,7 +3,6 @@ package DropdownScripts;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,45 +10,36 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-
 public class AscendingAndDescendingOrder {
-	
-	
-	// ArrayList > Collection
-	// How to dropdown
-	// Collections
-	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static void main(String[] args) {
-	  
-	  System.setProperty("webdriver.chrome.driver", "/............................./chromedriver");
-	  
-	  WebDriver driver = new ChromeDriver();
-	  driver.get("http://demo.guru99.com/test/newtours/register.php");
-	  
-	  Select country = new Select(driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[11]/td[2]/select")));
-	  
-	  //Actual List
-	  List actualList = new ArrayList();
-	  
-	  List <WebElement> countries = country.getOptions();
-	  
-	  //Capture WebElement Text
-	  for(WebElement ele:countries)
-	  {
-	      String data = ele.getText();
-	      actualList.add(data);
-	  }
-	  
-	  //Temp List
-	  List temp = new ArrayList();
-	  
-	  temp.addAll(actualList);
-	  
-	  //Ascending
-	  Collections.sort(temp);
-	  
-	  Assert.assertTrue(actualList.equals(temp));
   
-}
+  // ArrayList > Collection
+  // How to dropdown
+  // Collections
+  
+  @SuppressWarnings({ "unchecked", "rawtypes" })
+  public static void main(String[] args) {
+    System.setProperty("webdriver.chrome.driver", "/............................./chromedriver");
+    WebDriver driver = new ChromeDriver();
+    driver.get("http://demo.guru99.com/test/newtours/register.php");
+    
+    Select country = new Select(driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[11]/td[2]/select")));
+    
+    //Actual List
+    List actualList = new ArrayList();
+    List <WebElement> countries = country.getOptions();
+    
+    //Capture WebElement Text
+    for(WebElement ele:countries)
+    {
+      String data = ele.getText();
+      actualList.add(data);
+    }
+    //Temp List
+    List temp = new ArrayList();
+    temp.addAll(actualList);
+    
+    //Ascending
+    Collections.sort(temp);
+    Assert.assertTrue(actualList.equals(temp));
+  }
 }
